04221fefdba4d419ab81a6bfcfcc9789262593e8
Justin Chandler <justin.wchandler@gmail.com>
2018-03-22 19:56:06 +0000

    add add-as-a-service

---
+++ nothing/to/see/here/manual-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+BUCKET_NAME=aws-codestar-[region-name]-[account-id]
+BUCKET_NAME=aws-codestar-us-east-1-602437661913
+STACK_NAME=serverless-lab
+
+aws cloudformation package \
+    --template-file ./template.yml \
+    --s3-bucket $BUCKET_NAME \
+    --s3-prefix manual-deploy \
+    --output-template-file ./template.output.yml \
+    --force-upload 
+
+aws cloudformation deploy \
+    --template-file ./template.output.yml \
+    --stack-name $STACK_NAME\
+    --capabilities CAPABILITY_IAM
+    # --profile=$AWS_CRED_PROFILE
